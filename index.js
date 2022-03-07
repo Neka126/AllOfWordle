c74d621252396d307c0784f0540f774670cd50cb
FrostyAnimations126 <39423395+FrostyAnimations126@users.noreply.github.com>
2022-03-07 10:12:26 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 d=new Date()
 ld=new Date(d.getMonth()+1+"/"+(d.getDate()-1)+"/"+d.getFullYear())
-tw.innerText=list[d.toDateString()]
-yw.innerText=list[ld.toDateString()]
+tw.innerText=neka.Wordle.list[d.toDateString()]
+yw.innerText=neka.Wordle.list[ld.toDateString()]
