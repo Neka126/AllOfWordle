f03ddbca3aea17caf87be5a563a284e76509fd23
FrostyAnimations126 <39423395+FrostyAnimations126@users.noreply.github.com>
2022-03-07 10:08:59 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 d=new Date()
 ld=new Date(d.getMonth()+1+"/"+(d.getDate()-1)+"/"+d.getFullYear())
-list={
-  '2022-03-07T14:58:15.592Z':"Today's Word"
-}
-
-tw=list[d.toDateString()]
-yw=list[ld.toDateString()]
+tw.innerText=list[d.toDateString()]
+yw.innerText=list[ld.toDateString()]
