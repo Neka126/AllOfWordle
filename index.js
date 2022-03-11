573e0dc2d3e207ef43055e01572ada4c4b495272
FrostyAnimations126 <39423395+FrostyAnimations126@users.noreply.github.com>
2022-03-11 13:41:34 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ td=new Date(d.getMonth()+1+"/"+(d.getDate()+1)+"/"+d.getFullYear())
 tl=neka.Wordle.list[d.toDateString()]
 ts=neka.Wordle.list[ld.toDateString()]
 tts=neka.Wordle.list[td.toDateString()]
-ttw.innerText=`<div class="RowL-letter letter-correct">${tts[0]}</div><div class="RowL-letter letter-correct">${tts[1]}</div><div class="RowL-letter letter-correct">${tts[2]}</div><div class="RowL-letter letter-correct">${tts[3]}</div><div class="RowL-letter letter-correct">${tts[4]}</div>`
-tw.innerText=`<div class="RowL-letter letter-correct">${ts[0]}</div><div class="RowL-letter letter-correct">${ts[1]}</div><div class="RowL-letter letter-correct">${ts[2]}</div><div class="RowL-letter letter-correct">${ts[3]}</div><div class="RowL-letter letter-correct">${ts[4]}</div>`
-yw.innerText=`<div class="RowL-letter letter-correct">${ys[0]}</div><div class="RowL-letter letter-correct">${ys[1]}</div><div class="RowL-letter letter-correct">${ys[2]}</div><div class="RowL-letter letter-correct">${ys[3]}</div><div class="RowL-letter letter-correct">${ys[4]}</div>`
+ttw.innerText=`<span class="RowL-letter letter-correct">${tts[0]}</span><span class="RowL-letter letter-correct">${tts[1]}</span><span class="RowL-letter letter-correct">${tts[2]}</span><span class="RowL-letter letter-correct">${tts[3]}</span><span class="RowL-letter letter-correct">${tts[4]}</span>`
+tw.innerText=`<span class="RowL-letter letter-correct">${ts[0]}</span><span class="RowL-letter letter-correct">${ts[1]}</span><span class="RowL-letter letter-correct">${ts[2]}</span><span class="RowL-letter letter-correct">${ts[3]}</span><span class="RowL-letter letter-correct">${ts[4]}</span>`
+yw.innerText=`<span class="RowL-letter letter-correct">${ys[0]}</span><span class="RowL-letter letter-correct">${ys[1]}</span><span class="RowL-letter letter-correct">${ys[2]}</span><span class="RowL-letter letter-correct">${ys[3]}</span><span class="RowL-letter letter-correct">${ys[4]}</span>`
