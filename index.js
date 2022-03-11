57b9947ce19b370424124634eb6a3e38f2674ae9
FrostyAnimations126 <39423395+FrostyAnimations126@users.noreply.github.com>
2022-03-11 13:43:52 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 d=new Date()
 ld=new Date(d.getMonth()+1+"/"+(d.getDate()-1)+"/"+d.getFullYear())
 td=new Date(d.getMonth()+1+"/"+(d.getDate()+1)+"/"+d.getFullYear())
-tl=neka.Wordle.list[d.toDateString()]
-ts=neka.Wordle.list[ld.toDateString()]
+ys=neka.Wordle.list[ld.toDateString()]
+ts=neka.Wordle.list[d.toDateString()]
 tts=neka.Wordle.list[td.toDateString()]
-ttw.innerText=`<span class="RowL-letter letter-correct">${tts[0]}</span><span class="RowL-letter letter-correct">${tts[1]}</span><span class="RowL-letter letter-correct">${tts[2]}</span><span class="RowL-letter letter-correct">${tts[3]}</span><span class="RowL-letter letter-correct">${tts[4]}</span>`
-tw.innerText=`<span class="RowL-letter letter-correct">${ts[0]}</span><span class="RowL-letter letter-correct">${ts[1]}</span><span class="RowL-letter letter-correct">${ts[2]}</span><span class="RowL-letter letter-correct">${ts[3]}</span><span class="RowL-letter letter-correct">${ts[4]}</span>`
-yw.innerText=`<span class="RowL-letter letter-correct">${ys[0]}</span><span class="RowL-letter letter-correct">${ys[1]}</span><span class="RowL-letter letter-correct">${ys[2]}</span><span class="RowL-letter letter-correct">${ys[3]}</span><span class="RowL-letter letter-correct">${ys[4]}</span>`
+ttw.innerText=tts//`<span class="RowL-letter letter-correct">${tts[0]}</span><span class="RowL-letter letter-correct">${tts[1]}</span><span class="RowL-letter letter-correct">${tts[2]}</span><span class="RowL-letter letter-correct">${tts[3]}</span><span class="RowL-letter letter-correct">${tts[4]}</span>`
+tw.innerText=ts//`<span class="RowL-letter letter-correct">${ts[0]}</span><span class="RowL-letter letter-correct">${ts[1]}</span><span class="RowL-letter letter-correct">${ts[2]}</span><span class="RowL-letter letter-correct">${ts[3]}</span><span class="RowL-letter letter-correct">${ts[4]}</span>`
+yw.innerText=ys//`<span class="RowL-letter letter-correct">${ys[0]}</span><span class="RowL-letter letter-correct">${ys[1]}</span><span class="RowL-letter letter-correct">${ys[2]}</span><span class="RowL-letter letter-correct">${ys[3]}</span><span class="RowL-letter letter-correct">${ys[4]}</span>`
